3f3465fd639501439f6e1fe4b76791b8ba6a34a6
Anders Ravn <aravn4@gmail.com>
2022-11-30 16:18:54 +0100

    Added endpoints

---
+++ Controllers/FoodController.cs
@@ -0,0 +1,39 @@
+﻿using co2app.Models;
+using co2app.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace co2app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodItemsController : ControllerBase
+    {
+        private readonly ILogger<FoodItemsController> _logger;
+        private readonly FoodService _foodService;
+
+        public FoodItemsController(ILogger<FoodItemsController> logger, FoodService foodService)
+        {
+            _logger = logger;
+            _foodService = foodService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Food> Get()
+        {
+            return _foodService.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Food> GetFoodItem(int id)
+        {
+            var foodItem = _foodService.Get(id);
+
+            if (foodItem == null)
+            {
+                return NotFound();
+            }
+            return foodItem;
+        }
+    }
+}
